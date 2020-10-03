2989e22bd0917ae39530a6ba55c657223f8f5075
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-10-03 16:32:05 +0100

    project setup

---
+++ src/store.js
@@ -0,0 +1,19 @@
+export default {
+  destinations: [
+    {
+      name: "Brazil",
+      slug: "brazil",
+      image: "brazil",
+      id: 1,
+      description: "all about Brazil,",
+      experiences: [
+        {
+          name: "Iguacu Falls",
+          slug: "iguacu-falls",
+          image: "iguacu-falls",
+          description: "sdfghjkcvbnm",
+        },
+      ],
+    },
+  ],
+};
